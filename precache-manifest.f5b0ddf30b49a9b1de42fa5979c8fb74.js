8a51a6797620507bc5432c2b7f68de500a30fea3
Gaurav Gupta <gaurav.g@glowroad.com>
2020-04-12 13:22:39 +0530

    Updates

---
+++ precache-manifest.f5b0ddf30b49a9b1de42fa5979c8fb74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a28a0c7c0e5d9a8611c133deed6a9c6b",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "f71d861d862d73b4e6c5",
+    "url": "/Portfolio/static/css/main.e2473e87.chunk.css"
+  },
+  {
+    "revision": "516c8efae3965f7f2e01",
+    "url": "/Portfolio/static/js/2.79ff50a4.chunk.js"
+  },
+  {
+    "revision": "f71d861d862d73b4e6c5",
+    "url": "/Portfolio/static/js/main.5b9dba4e.chunk.js"
+  },
+  {
+    "revision": "4630f2122556b98468c4",
+    "url": "/Portfolio/static/js/runtime-main.760fdbd4.js"
+  }
+]);
